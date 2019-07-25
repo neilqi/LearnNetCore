e9250a3a04d6f7454bd8a022d88ac06b23d8159f
neil_qi <neil_qi@126.com>
2019-07-25 16:33:22 +0800

    Razor语法和VS操作，对应第5、6章

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Product myProd = new Product
+            //{
+            //    ProductID = 1,
+            //    Name = "kayak",
+            //    Description = "A boat for one person",
+            //    Category = "Watersports",
+            //    Price = 275M
+            //};
+
+            //ViewBag.StockLevel = 2;
+
+            //return View(myProd);
+
+            Product[] array = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            return View(array);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WorkingWithVS/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVS.Models;
+
+namespace WorkingWithVS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+            => View(SimpleRepository.SharedRepository.Products
+                .Where(p => p?.Price < 50));
+
+    }
+}
