c824531f85ba9cdf09efb2a069616ec8a29e2a18
neil_qi <neil_qi@126.com>
2019-07-25 18:05:07 +0800

    单元测试

---
+++ UnitTest/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnitTest.Models;
+
+namespace UnitTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
+        public IActionResult Index() => View(Repository.Products);
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UnitTest/Models/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace UnitTest.Models
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+        void AddProduct(Product p);
+    }
+}
